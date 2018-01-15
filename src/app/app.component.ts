92e97724b56fa4781f3d0a7146de3e5e2bf76db9
Philip Cesar Garay <pi.philipcesar@gmail.com>
2018-01-15 17:52:11 +0800

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,229 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+
+declare let THREE: any;
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+	title = 'app';
+	appModels:any ="";
+
+	constructor() {
+
+	}
+
+	ngOnInit() {
+		console.log('ngInit')
+		this.initGUI();
+	}
+
+	initGUI() {
+		var color = 0x000000;
+		var group = new THREE.Group();
+		var objects: any[] = [];
+
+		let innerW = document.getElementById('rendererDiv').offsetWidth;
+		let innerH = document.getElementById('rendererDiv').offsetHeight;
+
+		let scene = new THREE.Scene();
+		let camera = new THREE.PerspectiveCamera(75, innerW / window.innerHeight, 0.1, 1000);
+		camera.position.z = 100;
+		camera.position.x = 400;
+		camera.position.y = 150;
+
+		var light = new THREE.PointLight(0xEEEEEE);
+		light.position.set(20, 0, 20);
+		scene.add(light);
+
+		var lightAmb = new THREE.AmbientLight(0x777777);
+		scene.add(lightAmb);
+
+		let renderer = new THREE.WebGLRenderer();
+		renderer.setSize(innerW, window.innerHeight);
+		document.getElementById('renderHere').appendChild(renderer.domElement);
+
+		var backgroundMesh = new THREE.Mesh(
+			new THREE.PlaneGeometry(10, 10, 10, 10),
+			new THREE.MeshBasicMaterial({
+				map: modelChair()
+			})
+		);
+
+		backgroundMesh.material.depthTest = false;
+		backgroundMesh.material.depthWrite = true;
+
+		var backgroundScene = new THREE.Scene();
+		var backgroundCamera = new THREE.Camera();
+		backgroundScene.add(backgroundCamera);
+		backgroundScene.add(backgroundMesh);
+
+		window.addEventListener('resize', onWindowResize, false);
+
+		function modelChair() {
+			var mtlLoaderChair = new THREE.MTLLoader();
+			mtlLoaderChair.setBaseUrl('assets/models/chair/');
+			mtlLoaderChair.setPath('assets/models/chair/');
+			mtlLoaderChair.load('chair.mtl', function(materials) {
+
+				materials.preload();
+
+				materials.materials.fusta_taula.map.magFilter = THREE.NearestFilter;
+				materials.materials.fusta_taula.map.minFilter = THREE.LinearFilter;
+
+				var objLoaderChair = new THREE.OBJLoader();
+				objLoaderChair.setMaterials(materials);
+				objLoaderChair.setPath('assets/models/chair/');
+				objLoaderChair.load('chair.obj', function(object) {
+					object.position.y = -40;
+					object.position.z = 330;
+					object.scale.set(80, 80, 80);
+					// object.rotation.x = .01;
+					scene.add(object);
+				});
+
+			});
+		};
+
+		function modelBed() {
+			var mtlLoaderBed = new THREE.MTLLoader();
+			mtlLoaderBed.setBaseUrl('assets/models/bed/');
+			mtlLoaderBed.setPath('assets/models/bed/');
+			mtlLoaderBed.load('juniorBed.mtl', function(materials) {
+
+				materials.preload();
+				materials.materials.Wood.map.magFilter = THREE.NearestFilter;
+				materials.materials.Wood.map.minFilter = THREE.LinearFilter;
+
+				var objLoaderBed = new THREE.OBJLoader();
+				objLoaderBed.setMaterials(materials);
+				objLoaderBed.setPath('assets/models/bed/');
+				objLoaderBed.load('juniorBed.obj', function(object) {
+					object.position.x = 35;
+					object.position.y = -43;
+					object.position.z = 230;
+					object.scale.set(1, 1, 1);
+					object.rotation.x = .01;
+					object.rotation.y = -4.7;
+					scene.add(object);
+
+				});
+			});
+		};
+
+		function modelCycle() {
+			var mtlLoaderBed = new THREE.MTLLoader();
+			mtlLoaderBed.setBaseUrl('assets/models/cycle/');
+			mtlLoaderBed.setPath('assets/models/cycle/');
+			mtlLoaderBed.load('cycle_model_01.mtl', function(materials) {
+				materials.preload();
+				var objLoaderBed = new THREE.OBJLoader();
+				objLoaderBed.setMaterials(materials);
+				objLoaderBed.setPath('assets/models/cycle/');
+				objLoaderBed.load('cycle_model_01.obj', function(object) {
+					object.position.x = 35;
+					object.position.y = 112;
+					object.scale.set(15, 15, 15);
+					scene.add(object);
+
+				});
+
+			});
+		};
+
+		function modelHouse() {
+			var mtlLoader = new THREE.MTLLoader();
+			mtlLoader.setBaseUrl('assets/models/house/');
+			mtlLoader.setPath('assets/models/house/');
+			mtlLoader.load('house interior.mtl', function(materials) {
+				materials.preload();
+				var objLoader = new THREE.OBJLoader();
+				objLoader.setMaterials(materials);
+				objLoader.setPath('assets/models/house/');
+				objLoader.load('house interior.obj', function(object) {
+					object.position.x = 0;
+					object.position.y = -40;
+					object.position.z = 0;
+					object.scale.set(1, 1, 1);
+					scene.add(object);
+				});
+
+			});
+		};
+
+		function modelOfficeChair() {
+			var mtlLoaderOfficeChair = new THREE.MTLLoader();
+			mtlLoaderOfficeChair.setBaseUrl('assets/models/office_chair/');
+			mtlLoaderOfficeChair.setPath('assets/models/office_chair/');
+			mtlLoaderOfficeChair.load('office_chair.mtl', function(materials) {
+				materials.preload();
+				var objLoaderOfficeChair = new THREE.OBJLoader();
+				objLoaderOfficeChair.setMaterials(materials);
+				objLoaderOfficeChair.setPath('assets/models/office_chair/');
+				objLoaderOfficeChair.load('office_chair.obj', function(object) {
+					object.position.x = -10;
+					object.scale.set(70, 70, 70);
+					object.position.y = -40;
+					object.position.z = 270;
+					object.rotation.x = .01;
+					object.rotation.y = -4.7;
+					object.updateMatrix();
+					group.add(object);
+
+					scene.add(object);
+					objects.push(group);
+				});
+			});
+		};
+
+
+		function onWindowResize() {
+			camera.aspect = innerW / window.innerHeight;
+			camera.updateProjectionMatrix();
+			renderer.setSize(innerW, window.innerHeight);
+		}
+
+
+		function control() {
+			let controls = new THREE.OrbitControls(camera, renderer.domElement);
+			controls.enableDamping = true;
+			controls.dampingFactor = 0.25;
+			controls.enableZoom = true;
+			controls.minPolarAngle = 0; // radians
+			controls.maxPolarAngle = Math.PI; // radians
+			controls.minAzimuthAngle = 0; // radians
+			controls.maxAzimuthAngle = Math.PI; // radians
+		};
+
+		control();
+
+		let animate = function() {
+			requestAnimationFrame(animate);
+
+			if (color < 0xdddddd) color += 0x0000ff;
+			renderer.autoClear = false;
+			renderer.clear();
+			renderer.render(backgroundScene, backgroundCamera);
+			renderer.render(scene, camera);
+		};
+
+		animate();
+	}
+
+	selectModel(){
+		console.log('selectModel: ', this.appModels);
+		if(this.appModels === 'chair'){
+			
+		}else if(this.appModels === 'officechair'){
+
+		}else if(this.appModels === 'bed'){
+
+		}else if(this.appModels === 'cycle'){
+
+		}else{
+			
+		}
+	}
 }
